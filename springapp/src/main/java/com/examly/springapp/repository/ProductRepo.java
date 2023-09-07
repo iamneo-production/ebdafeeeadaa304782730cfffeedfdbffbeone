e3e57909d5f6f26545830353a53a6cb98f81c335
Iamneo <techadmin@iamneo.ai>
2023-09-07 10:31:05 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/ProductRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/ProductRepo.java
@@ -2,6 +2,8 @@ package com.examly.springapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.examly.springapp.model.Product;
+import java.util.Optional;
+import java.util.*;
 
 public interface ProductRepo  extends JpaRepository<Product,Integer>{
     List<Product> findByCategory_Id(int categoryId);
